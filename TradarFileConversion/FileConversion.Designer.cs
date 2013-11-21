c97b913e9197ec83ff9b58a2ee5a95eeb8a6c96e
erbas <thompson.keiran@gmail.com>
2013-11-21 19:37:47 +0800

    App to convert NT7 trade files into Tradar format

---
+++ TradarFileConversion/FileConversion.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace TradarFileConversion
+{
+    partial class FileConversion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ButtonGo = new System.Windows.Forms.Button();
+            this.Quit = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(100, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(381, 206);
+            this.textBox1.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Title = "File";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.ButtonFile_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.folderBrowserDialog1.SelectedPath = "C:\\TMP";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(8, 244);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Folder";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.ButtonFolder_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(100, 244);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(381, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // ButtonGo
+            // 
+            this.ButtonGo.Location = new System.Drawing.Point(8, 295);
+            this.ButtonGo.Name = "ButtonGo";
+            this.ButtonGo.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGo.TabIndex = 4;
+            this.ButtonGo.Text = "Go";
+            this.ButtonGo.UseVisualStyleBackColor = true;
+            this.ButtonGo.Click += new System.EventHandler(this.ButtonGo_Click);
+            // 
+            // Quit
+            // 
+            this.Quit.Location = new System.Drawing.Point(406, 295);
+            this.Quit.Name = "Quit";
+            this.Quit.Size = new System.Drawing.Size(75, 23);
+            this.Quit.TabIndex = 5;
+            this.Quit.Text = "Quit";
+            this.Quit.UseVisualStyleBackColor = true;
+            this.Quit.Click += new System.EventHandler(this.ButtonQuit_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.Control;
+            this.progressBar1.Location = new System.Drawing.Point(8, 324);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(473, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 6;
+            // 
+            // FileConversion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 356);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Quit);
+            this.Controls.Add(this.ButtonGo);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "FileConversion";
+            this.Text = "Tradar File Conversion Utility";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button ButtonGo;
+        private System.Windows.Forms.Button Quit;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ TradarFileConversion/FileConversion.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TradarFileConversion
+{
+    public partial class FileConversion : Form
+    {
+        String[] input_files = new String[] {""};
+        String path_out = "";
+
+        public FileConversion()
+        {
+            InitializeComponent();
+            progressBar1.Style = ProgressBarStyle.Continuous;
+            progressBar1.Maximum = 100;
+            progressBar1.Value = 0;
+        }
+
+        private void ButtonFile_Click(object sender, EventArgs e)           // select NT7 trade files to convert 
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            input_files = new string[openFileDialog1.FileNames.Length];
+            input_files = openFileDialog1.FileNames;
+
+            textBox1.WordWrap = true;
+            textBox1.Text = String.Join(",", input_files);
+
+        }
+
+        private void ButtonFolder_Click(object sender, EventArgs e)         // select destination folder
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            path_out = folderBrowserDialog1.SelectedPath.Replace(@"\", "/"); ;
+            // tidy up output path
+            if (path_out.Last() != '/')
+            {
+                path_out = path_out + '/';
+            };
+            textBox2.Text = path_out;
+
+        }
+
+        private void ButtonQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ButtonGo_Click(object sender, EventArgs e)         // do the file conversion
+        {
+            double inc = 100/input_files.Length;
+            int n = 1;
+            foreach (string filename in input_files) 
+            {
+                DoNT7toTradar(filename, path_out);
+                progressBar1.Value = (int)(n * inc);
+                progressBar1.Update();
+                n++;
+            }
+
+        }
+
+        private void DoNT7toTradar(string filename, string path_out)
+        {
+
+            string nt7filename = System.IO.Path.GetFileNameWithoutExtension(filename);
+
+          //  Trade-#,Instrument,Account,Strategy,Market pos.,Quantity,Entry price,Exit price,Entry time,Exit time,Entry name,Exit name,Profit,Cum. profit,Commission,MAE,MFE,ETD,Bars,
+            Dictionary<string, int> nt7 = new Dictionary<string, int>();
+
+            nt7["Trade Number"] = 0;
+            nt7["Instrument"] = 1;
+            nt7["Account"] = 2;
+            nt7["Strategy"] = 3;
+            nt7["Market pos."] = 4;
+            nt7["Quantity"] = 5;
+            nt7["Entry price"] = 6;
+            nt7["Exit price"] = 7;
+            nt7["Entry time"] = 8;
+            nt7["Exit time"] = 9;
+            nt7["Entry name"] = 10;
+            nt7["Exit name"] = 11;
+            nt7["Profit"] = 12;
+
+            Dictionary<string, int> tradar = new Dictionary<string, int>();
+
+            tradar["Trade Type"] = 0;
+            tradar["Security Identifier"] = 1;
+            tradar["Strategy"] = 2;
+            tradar["Fund"] = 3;
+            tradar["Trader"] = 4;
+            tradar["Price"] = 5;
+            tradar["Amount"] = 6;
+            tradar["Trade Date"] = 7;
+            tradar["Consideration"] = 8;
+
+            // define headings for output file
+            // NOTE: using 'fund' for trade reference
+            string tradar_headers =  "Trade Type, Security Identifier, Strategy, Amount, Price, Trade Date, Fund";
+
+            // need to reverse position for closing trades
+            Dictionary<string, string> nt7_close = new Dictionary<string, string>();
+            nt7_close.Add("Long", "Short");
+            nt7_close.Add("Short", "Long");
+
+            // translate NT7 trade types into tradar types
+            Dictionary<string, string> trade_type = new Dictionary<string, string>();
+            trade_type.Add("Long", "Buy");
+            trade_type.Add("Short", "Sell");
+
+            // use a hash of the full filename with path to get an integer reference linking buys and sells
+            string filename_hash = filename.GetHashCode().ToString();
+
+            // open the output file
+            string tradar_trade_filename = System.IO.Path.Combine(new string [] {path_out, nt7filename + "_tradar.csv"});
+            System.IO.StreamWriter tradar_trade_file = new System.IO.StreamWriter(tradar_trade_filename);
+            tradar_trade_file.WriteLine(tradar_headers);
+            
+            // debug
+            Console.WriteLine("about to process");
+
+            foreach (string line in System.IO.File.ReadLines(filename))
+            {
+                string[] s = line.Split(',');
+                if (s[0] == "Trade-#")
+                {
+                    continue;  // skip the first line
+                }
+                string security_identifier = s[nt7["Instrument"]].Trim(new char[] {'$'});
+                string trade_ref = filename_hash + '_' + s[nt7["Trade Number"]];
+                string strategy = nt7filename;
+
+                string s_tradar_1 = String.Join(",", new string [] {   
+                                                trade_type[s[nt7["Market pos."]]], 
+                                                security_identifier, 
+                                                nt7filename,  
+                                                s[nt7["Quantity"]],
+                                                s[nt7["Entry price"]],
+                                                s[nt7["Entry time"]],
+                                                trade_ref
+                                            });
+
+                string s_tradar_2 = String.Join(",", new string [] {   
+                                                trade_type[nt7_close[s[nt7["Market pos."]]]], 
+                                                security_identifier, 
+                                                nt7filename,  
+                                                s[nt7["Quantity"]],
+                                                s[nt7["Exit price"]],
+                                                s[nt7["Exit time"]],
+                                                trade_ref
+                                            });
+
+
+                //Console.WriteLine(s_tradar_1);
+                //Console.WriteLine(s_tradar_2);
+                tradar_trade_file.WriteLine(s_tradar_1);
+                tradar_trade_file.WriteLine(s_tradar_2);
+            }
+
+            // debug
+            Console.WriteLine("done processing");
+
+            tradar_trade_file.Close();
+
+        }
+
+    }
+}
