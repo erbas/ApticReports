e981c829ef24ebd9ad6c5eca6f69f60eab3c90c5
erbas <thompson.keiran@gmail.com>
2013-12-19 15:38:27 +1100

    Fix path to benchmarks

--- R src/PortfolioMakeReport.R
+++ R src/PortfolioMakeReport.R
@@ -20,7 +20,7 @@ path.src <- paste0(Sys.getenv("HOME"),"/GitRepo/ApticReports/R src/")
 # print(filenames)
 
 #load benchmark files
-index.path <- "E:/Data History/Benchmarks Indices/Other/"
+index.path <- "E:/Cloud Data/Data History/Benchmarks Indices/Other/"
 new.edge.csv <- read.csv(paste0(index.path,"NewEdge_CTA_Historical.csv"),header=F,stringsAsFactors=F)
 vals <- as.numeric(strsplit(new.edge.csv[,2],"%"))/100
 new.edge.xts <- xts(vals,as.Date(new.edge.csv[,1],format="%d/%m/%Y",tz="Europe/London"))
